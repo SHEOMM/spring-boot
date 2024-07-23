cd330479baf7a9442a519c7f8d4723484624033b
SHEOMM <esh0821@snu.ac.kr>
2024-07-23 20:04:07 +0900

    feat: 메모리 조회 API 구현

---
+++ memory/memory-v1/src/test/java/MemoryFinderTest.java
@@ -0,0 +1,14 @@
+import memory.Memory;
+import memory.MemoryFinder;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemoryFinderTest {
+    @Test
+    void get() {
+        MemoryFinder memoryFinder = new MemoryFinder();
+        Memory memory = memoryFinder.get();
+        assertThat(memory).isNotNull();
+    }
+}
